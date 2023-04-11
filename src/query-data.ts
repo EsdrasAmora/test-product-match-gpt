c36e8a27996faa9db7ae439b831fd1ceb07c36d1
EsdrasAmora <esdras.amora@gmail.com>
2023-04-10 23:47:25 -0300

    temp33

--- src/query-data.ts
+++ src/query-data.ts
@@ -9,16 +9,35 @@ const client = weaviate.client({
 });
 
 client.graphql
-  .get()
+  // .get()
+  .aggregate()
   .withClassName("Product")
-  .withFields("category description segments supplier")
-  .withNearText({ concepts: ["ovos"] })
-  .withLimit(10)
+  .withFields("meta { count }")
+  // .withFields("meta { count } ")
+  // .withNearText({ concepts: ["calorico"] })
+  // .withObjectLimit(10)
+  // .withSort([{ path: ["description"], order: "asc" }])
+  // .withLimit(10)
+  // .withGroup({ force: 0.2, type: "description" })
   .do()
   .then((res: any) => {
     console.log("good", res);
-    console.dir(res.data.Get.Product, { depth: 100 });
-  })
-  .catch((err: Error) => {
-    console.error("bad", err);
+    console.dir(res.data.Aggregate.Product, { depth: 100 });
+    // console.dir(res.data.Get.Product, { depth: 100 });
   });
+
+// client.graphql
+//   .get()
+//   .withClassName("Product")
+//   // .withGroup({ force: 0.1, type: "text2vec-openai" })
+//   // .withFields("category description segments supplier")
+//   // .withNearText({ concepts: ["ovos"] })
+//   // .withLimit(10)
+//   .do()
+//   .then((res: any) => {
+//     console.log("good", res);
+//     console.dir(res.data.Get.Product, { depth: 100 });
+//   })
+//   .catch((err: Error) => {
+//     console.error("bad", err);
+//   });
