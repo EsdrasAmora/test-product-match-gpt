fbb3aa843b894662feaae7b8b84f2a43c10fa8a5
EsdrasAmora <esdras.amora@gmail.com>
2023-04-11 10:33:52 -0300

    tempgg

--- src/query-data.ts
+++ src/query-data.ts
@@ -9,35 +9,22 @@ const client = weaviate.client({
 });
 
 client.graphql
-  // .get()
-  .aggregate()
+  .get()
   .withClassName("Product")
-  .withFields("meta { count }")
-  // .withFields("meta { count } ")
-  // .withNearText({ concepts: ["calorico"] })
-  // .withObjectLimit(10)
-  // .withSort([{ path: ["description"], order: "asc" }])
-  // .withLimit(10)
-  // .withGroup({ force: 0.2, type: "description" })
+  .withFields("category description segments supplier _additional { distance }")
+  .withLimit(10)
   .do()
   .then((res: any) => {
     console.log("good", res);
-    console.dir(res.data.Aggregate.Product, { depth: 100 });
-    // console.dir(res.data.Get.Product, { depth: 100 });
+    console.dir(res.data.Get.Product, { depth: 100 });
   });
 
 // client.graphql
-//   .get()
+//   .aggregate()
 //   .withClassName("Product")
-//   // .withGroup({ force: 0.1, type: "text2vec-openai" })
-//   // .withFields("category description segments supplier")
-//   // .withNearText({ concepts: ["ovos"] })
-//   // .withLimit(10)
+//   .withFields("meta { count }")
 //   .do()
 //   .then((res: any) => {
 //     console.log("good", res);
 //     console.dir(res.data.Get.Product, { depth: 100 });
-//   })
-//   .catch((err: Error) => {
-//     console.error("bad", err);
 //   });
--- src/update-schema.ts
+++ src/update-schema.ts
@@ -12,16 +12,16 @@ async function main() {
   const schemaRes = await client.schema.getter().do();
 
   console.log("here", schemaRes);
-  //   const schemaConfig = {
-  //     class: "Product",
-  //     vectorizer: "text2vec-openai",
-  //   };
+  const schemaConfig = {
+    class: "Product",
+    vectorizer: "text2vec-openai",
+  };
 
-  //   const response = await client.schema
-  //     .classCreator()
-  //     .withClass(schemaConfig)
-  //     .do();
-  //   console.log("response", response);
+  const response = await client.schema
+    .classCreator()
+    .withClass(schemaConfig)
+    .do();
+  console.log("response", response);
 }
 
 void main();
